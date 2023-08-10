ddbceeeca27b19d40830d3b826a2f1129a77c13a
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-08-10 15:19:42 -0700

    Create function for choosing number of die dots

---
+++ src/components/Dots.js
@@ -0,0 +1,15 @@
+import One from "./One";
+import Two from "./Two";
+import Three from "./Three";
+import Four from "./Four";
+import Five from "./Five";
+import Six from "./Six";
+
+export default function useDots({ roll }) {
+  if (roll === 1) return <One />;
+  if (roll === 2) return <Two />;
+  if (roll === 3) return <Three />;
+  if (roll === 4) return <Four />;
+  if (roll === 5) return <Five />;
+  if (roll === 6) return <Six />;
+}
