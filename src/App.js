ad0139848c3ef3f8fa6c3fcab2ee5a9b660a247a
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-08-10 15:21:29 -0700

    Add Dots component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,20 @@
-import "./App.css";
 import React, { useState } from "react";
-import One from "./components/One";
-import Two from "./components/Two";
-import Three from "./components/Three";
-import Four from "./components/Four";
-import Five from "./components/Five";
-import Six from "./components/Six";
+import "./App.css";
+import Dots from "./components/Dots";
 
 export default function App() {
   const [rolledValue, setRolledValue] = useState(1);
 
   const roll = () => {
-    setRolledValue(Math.ceil(Math.random() * 5 + 1));
+    setRolledValue(Math.floor(Math.random() * 6) + 1);
   };
 
   return (
     <div className="main">
-      <p className="value">{rolledValue}</p>
+      {/* <p className="value">{rolledValue}</p> */}
       <button className="die" onClick={roll}>
-        <div className="dots top-left"></div>
-        <div className="dots center"></div>
-        <div className="dots bottom-right"></div>
-      </button>
-      <button className="die" onClick={roll}>
-        <Six />
+        <Dots roll={rolledValue} />
+        {/* {Dots({ roll: rolledValue })} */}
       </button>
     </div>
   );
