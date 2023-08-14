cf7652c714d05843df03b06e39014bf6f8077056
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-08-14 16:49:50 -0700

    Import dice components

--- src/App.js
+++ src/App.js
@@ -1,35 +1,13 @@
-import React, { useState } from "react";
 import "./App.css";
-import Dots from "./components/Dots";
-import d20Icon from "./images/d20-icon-grey.png";
+import D6 from "./components/D6/D6";
+import D20 from "./components/D20";
 
 export default function App() {
-  const [rolledValue, setRolledValue] = useState(1);
-  const [rolledValue20, setRolledValue20] = useState(1);
-
-  const roll = () => {
-    setRolledValue(Math.floor(Math.random() * 6) + 1);
-  };
-
-  const rollTwenty = () => {
-    setRolledValue20(Math.floor(Math.random() * 20) + 1);
-  };
-
   return (
     <div className="main">
       <h1 className="title">DICEY</h1>
-      <button className="die" onClick={roll}>
-        <Dots roll={rolledValue} />
-      </button>
-      <div className="twenty">
-        <img
-          alt="20-sided die"
-          src={d20Icon}
-          className="twenty-img"
-          onClick={rollTwenty}
-        />
-        <p className="twenty-roll">{rolledValue20}</p>
-      </div>
+      <D6 />
+      <D20 />
     </div>
   );
 }
