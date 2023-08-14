abfe86122f49400ca60507242f24668b68f3546d
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-08-14 16:22:29 -0700

    Add logic for twenty-sided die

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import React, { useState } from "react";
 import "./App.css";
 import Dots from "./components/Dots";
+import d20Icon from "./images/d20-icon-grey.png";
 
 export default function App() {
   const [rolledValue, setRolledValue] = useState(1);
+  const [rolledValue20, setRolledValue20] = useState(1);
 
   const roll = () => {
     setRolledValue(Math.floor(Math.random() * 6) + 1);
   };
 
+  const rollTwenty = () => {
+    setRolledValue20(Math.floor(Math.random() * 20) + 1);
+  };
+
   return (
     <div className="main">
+      <h1 className="title">DICEY</h1>
       <button className="die" onClick={roll}>
         <Dots roll={rolledValue} />
       </button>
+      <div className="twenty">
+        <img
+          alt="20-sided die"
+          src={d20Icon}
+          className="twenty-img"
+          onClick={rollTwenty}
+        />
+        <p className="twenty-roll">{rolledValue20}</p>
+      </div>
     </div>
   );
 }
