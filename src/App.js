86d4bfc86c4c5c0b871a28aa23c0645e0eed57fd
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-08-16 20:26:53 -0700

    Refactor to accomodate new components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import "./App.css";
-import D6 from "./components/D6/D6";
-import D20 from "./components/D20";
+import Die from "./components/Die";
 
 export default function App() {
   return (
     <div className="main">
       <h1 className="title">DICEY</h1>
-      <D6 />
-      <D20 />
+      <Die />
     </div>
   );
 }
